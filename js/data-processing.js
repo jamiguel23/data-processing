b900932adb0b9d4ec1b4b512834046f2c2e52822
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-12 11:30:52 -0800

    commit

--- js/data-processing.js
+++ js/data-processing.js
@@ -2,10 +2,16 @@
 
 const queryString = window.location.search;
 
-var urlParams = new URLSearchParams(queryString);
+if(queryString.length > 0){
+  var urlParams = new URLSearchParams(queryString);
+  let data = ""
+  // Log the values
+  urlParams.forEach((value, key) => {
+    key = key.split("_").join(" ");
+    // console.log(key, value);
+    data += `<p>${key}: ${value}</p>`
+  });
 
-// Log the values
-urlParams.forEach((value, key) => {
-  key = key.split("_").join(" ");
-  console.log(key, value);
-});
+  data += '<p><a href="index.html">CLEAR</a></p>'
+  document.getElementById('output').innerHTML = data
+}

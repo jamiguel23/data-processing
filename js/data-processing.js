8e1512299d5a58d02c1a85604bcfe05b6ec2d3f4
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-12 11:05:33 -0800

    commit

--- js/data-processing.js
+++ js/data-processing.js
@@ -1,8 +1,10 @@
-console.log('Hello word, inside data processing file')
+// console.log('Hello word, inside data processing file')
 
-var searchParams = new URLSearchParams("key1=value1&key2=value2&color=red&size=small");
+const queryString = window.location.search;
+
+var urlParams = new URLSearchParams(queryString);
 
 // Log the values
-searchParams.forEach((value, key) => {
-  console.log(value, key);
+urlParams.forEach((value, key) => {
+  console.log(key, value);
 });

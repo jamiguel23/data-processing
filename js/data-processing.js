4de9cc0757b1d5202f2d88cd5063da83bc2553b7
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-12 10:55:49 -0800

    added query string

--- js/data-processing.js
+++ js/data-processing.js
@@ -1,1 +1,8 @@
 console.log('Hello word, inside data processing file')
+
+var searchParams = new URLSearchParams("key1=value1&key2=value2&color=red&size=small");
+
+// Log the values
+searchParams.forEach((value, key) => {
+  console.log(value, key);
+});

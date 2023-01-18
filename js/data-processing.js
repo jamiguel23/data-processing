141706ee5dfc852fd9d33bbd161345f194c0589c
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-18 10:56:27 -0800

    commit

--- js/data-processing.js
+++ js/data-processing.js
@@ -2,14 +2,44 @@
 
 const queryString = window.location.search;
 
-if(queryString.length > 0){
+if (queryString.length > 0) {
   var urlParams = new URLSearchParams(queryString);
-  let data = ""
+  let data = "";
+  let total = 0;
+  data += "<h3> Cart Contents </h3>"
+  
   // Log the values
   urlParams.forEach((value, key) => {
-    key = key.split("_").join(" ");
-    // console.log(key, value);
-    data += `<p>${key}: ${value}</p>`
+    
+    if (key == "Cart") { //cart items
+      // alert("Cart Item: " + value)
+      
+      switch(value){
+        
+        case "Widget":
+          data += "Widget: $3.99 <br>"
+          total += 3.99
+        break;
+
+        case "Thingy":
+          data += "Widget: $1.99 <br>"
+          total += 1.99
+        break
+
+        case "Sprocket":
+          data += "Widget: $5.99 <br>"
+          total += 5.99
+        break
+      }
+
+      data = "total:" + total+ "<br>"
+
+    } else {
+      
+      key = key.split("_").join(" ");
+      data += `<p>${key}: ${value}</p>`
+    }
+
   });
 
   data += '<p><a href="index.html">CLEAR</a></p>'
